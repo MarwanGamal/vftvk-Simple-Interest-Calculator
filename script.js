f2c9cdc1e7e9b826ac71eee25593aabde3db2175
MarwanGamal <96267764+MarwanGamal@users.noreply.github.com>
2021-12-26 03:08:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var range = document.getElementById("intrestRate");
+var bubble = document.getElementById("bubble");
+var A = range.value;
+var amount = document.getElementById("amount");
+
+range.addEventListener("input", () => {
+  setBubble(range, bubble);
+});
+
+
+function setBubble(range, bubble) {
+  A = range.value;
+  bubble.innerHTML = A + "%";
 }
-        
+
+var select = document.getElementById('NoYears');
+var option = select.options[select.selectedIndex].value;
+
+function change() {
+  select = document.getElementById('NoYears');
+  option = select.options[select.selectedIndex].value;
+  amount = document.getElementById("amount")
+}
+
+var button = document.getElementById("submit");
+button.onclick = function myFunction() {
+if(amount.value==""){
+alert("Please enter a number");
+amount.focus();
+}
+else{
+  document.getElementById("deposit").value = "If you deposit " + amount.value;
+  document.getElementById("rate").value = "At intrest rate of " + A + "%";
+  document.getElementById("recive").value = "You recive an amount of " + ((parseInt(amount.value) * parseInt(A) / 100 * option) + parseInt(amount.value));
+  document.getElementById("year").value = "in the year " + (parseInt(option) + 2021);
+  }
+
+}
